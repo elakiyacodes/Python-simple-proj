51ea21fb0ad503c5436106d12a8d392b6dcdde92
Elakiya <77778008+elakiyacodes@users.noreply.github.com>
2022-08-25 07:53:31 +0530

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,21 @@
+"""With six children in tow, Catherine raced to the airport departing gate. This wasn't an easy task as the children
+had other priorities than to get to the gate. She knew that she was tight on time and the frustration came out as she
+yelled at the kids to keep up. They continued to test her, pretending not to listen and to move in directions that
+only slowed them down. They had no idea the wrath they were about to receive when Catherine made it to the gate only
+to be informed that they had all missed the plane. """
+
+number = input("number: ")
+adj = input("adj: ")
+Name = input("name: ")
+adj = input("adj: ")
+verb = input("verb: ")
+who = input("who ")
+verb = input("verb:")
+
+madlibs = f" With {number} children in tow, Catherine raced to the airport departing gate. This wasn't an {adj} task as the \
+had other priorities than to get to the gate. {Name} knew that she was tight on time and the frustration came out as she\
+yelled at the {adj} to keep up. They continued to test her, {verb} not to listen and to move in directions that \
+only slowed them down. They had no idea the wrath they were about to receive when {who} made it to the gate only \
+to be informed that they had all {verb} the plane."
+
+print(madlibs)
