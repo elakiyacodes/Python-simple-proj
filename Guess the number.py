8b30568ffd647d100ead4dbb89cfbe4817876303
Elakiya <77778008+elakiyacodes@users.noreply.github.com>
2022-08-26 20:24:56 +0530

    Update Guess the number.py

--- Guess the number.py
+++ Guess the number.py
@@ -1,1 +1,16 @@
+import random
 
+
+def guess(x):
+    random_number = random.randint(1, x)  # random.radiant -- is a function to pick random numbers
+    guess = 0
+
+    while guess != random_number:
+        guess = input(f'Guess a number between 1 and {x}')
+        print(guess)
+
+    guess(10)
+
+    
+    
+    # ---- incommplete ----
